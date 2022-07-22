dc22e449854dd5b6aa48056f7b9a13a0c5f5f37b
sambeth <sambethslim@gmail.com>
2022-07-22 00:50:21 +0000

    prep test cases for Driver actor

---
+++ src/test/scala/DriverSpec.scala
@@ -0,0 +1,24 @@
+import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class DriverSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
+
+  "Driver" must {
+    "be in router group" in {
+
+    }
+
+    "receive message as part of group" in {
+
+    }
+
+    "deregister from router group after receiving message" in {
+
+    }
+
+    "receieve OrderInfo" in {
+
+    }
+  }
+
+}
